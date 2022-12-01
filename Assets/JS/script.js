d68899fae907ceb72230087665a8af3f9c5ef930
wdiste <Wdiste49@gmail.com>
2022-12-01 12:41:29 -0500

    created psuedo-code and skeleton of html

---
+++ Assets/JS/script.js
@@ -0,0 +1,33 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+//  ---------  Psuedo  ---------
+//
+// I click the start button
+// --> the game starts
+// ------> the timer starts
+// ----------> if timer hits (0) 
+// --------------> go through incorrect answer code
+// ------> a question appears on screen
+// ------> 4 answer buttons appear on screen
+// I click on an answer
+// --> the answer is logged
+// --> result (right/wrong) is counted
+// ------> if wrong, subtract 10 seconds from the clock
+// --> result (right/wrong) is flashed under answers
+// --> another answer is shown
+// I finish the quiz
+// --> highscore page is shown
+// ------> your score is shown
+// ------> form with name to save score
+// --> previous highscores are shown
+// --> clear highscores button is shown
