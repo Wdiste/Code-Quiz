3dd567d3634be043fa11b3f1dbebaaf3542084ed
wdiste <Wdiste49@gmail.com>
2022-12-15 13:39:33 -0500

    highscores loaded into table

--- Assets/JS/high-script.js
+++ Assets/JS/high-script.js
@@ -1,6 +1,4 @@
-var highName = document.querySelector("#highscores-name");
-var highScore = document.querySelector('#highscores-scores');
-
+var highEl = document.querySelector("#highscores-element");
 
 var highScoreCount = localStorage.getItem('highScoreCount');
 var highscoresNameList = [];
@@ -10,14 +8,19 @@ console.log(highscoresNameList);
 
 for (let i = 0; i < highscoresNameList.length; i++) {
     // create name and score text elements
-    var newName = document.createElement('li');
-    var newScore = document.createElement('li');
+    let newRow = document.createElement('tr');
+    var newName = document.createElement('td');
+    var newScore = document.createElement('td');
+
+
+    // add row to the table
+    highEl.appendChild(newRow);
 
     // add name to the text area using the element of the array of saved names
     newName.textContent = highscoresNameList[i];
-    highName.appendChild(newName);
+    newRow.appendChild(newName);
 
     // add name to the text area using the element of the data of saved scores
     newScore.textContent = localStorage.getItem(highscoresNameList[i]);
-    highScore.appendChild(newScore);
+    newRow.appendChild(newScore);
 };
